ee85b2f7f3f0f18c9691a0a0c8c508aa8a39fec0
Mithun Biswas <mithunbiswas.me@gmail.com>
2026-09-25 12:54:09 +0600

    updates

--- src/config/base-url.js
+++ src/config/base-url.js
@@ -3,5 +3,5 @@
 // export const baseUriBackend = "https://backend.logicraftit.com/";
 export const baseUriBackend = "http://localhost:8005/";
 
-// API Base
+// API Base Url
 export const API_BASE_URL = baseUriBackend + "api/v1/";
